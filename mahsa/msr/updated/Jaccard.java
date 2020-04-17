e31269a519d01a55546c592bcaa22e5f457da1ea
mahsahadian <51335153+mahsahadian@users.noreply.github.com>
2020-04-16 22:58:07 -0400

    Jaccard Distance and CLutering
    if jaccard distance <= 0.5 = 0(group1)
    else = 1 (group 2)

--- mahsa/msr/updated/Jaccard.java
+++ mahsa/msr/updated/Jaccard.java
@@ -24,16 +24,16 @@ public class Jaccard {
     }
 
     private static void jaccard() throws Exception {
-        String file = "cleaned_total.xlsx";
-        String path = "C:/Users/mahsa/Desktop/Variability/Clean/";
+        String file = "NonRepeated_Repos-Forks-recovery.xlsx";
+        String path = "C:/Users/mahsa/Desktop/Variability/CleanOfClean/SmallGroup2/";
         List<Double> Repos_id = Pick_GeneralNumeric.pick_3(path + file, 0, 0, 1);
         List<String> repo_nameL = Pick_GeneralNext.pick2(path + file, 0, 1, 1);
-        List<String> owner_nameL = Pick_GeneralNext.pick2(path + file, 0, 3, 1);
+        List<String> owner_nameL = Pick_GeneralNext.pick2(path + file, 0, 2, 1);
         //List<Double> owner_idL = Pick_GeneralNumeric.pick_3(path + file, 0, 2, 1);
         //List<Double> ClosedPR = Pick_GeneralNumeric.pick_3(path + file, 0, 4, 1);
         //List<Double> MergedPReq = Pick_GeneralNumeric.pick_3(path + file, 0, 5, 1);
-        List<String> full_nameL = Pick_GeneralNext.pick2(path + file, 0, 4, 1);
-        List<Double> depth = Pick_GeneralNumeric.pick_3(path + file, 0, 2, 1);
+        List<String> full_nameL = Pick_GeneralNext.pick2(path + file, 0, 3, 1);
+        List<Double> depth = Pick_GeneralNumeric.pick_3(path + file, 0, 4, 1);
         List<Double> Commits = Pick_GeneralNumeric.pick_3(path + file, 0, 6, 1);
         List<Double> TotalDependencies = Pick_GeneralNumeric.pick_3(path + file, 0, 7, 1);
         List<Double> CommitsAfter = Pick_GeneralNumeric.pick_3(path + file, 0, 10, 1);
@@ -43,8 +43,8 @@ public class Jaccard {
         List<String> DependAfter_Details = Pick_GeneralNext.pick2(path + file, 0, 12, 1);
 
 
-        System.out.println(Repos_id.size()+","+repo_nameL.size()+", "+owner_nameL.size()+", "+full_nameL.size()+", "+depth.size()+", "+Commits.size()+","
-        +TotalDependencies.size()+", "+CommitsAfter.size()+", "+DependAfter.size()+", "+Created_at.size()+", "+DependAfter_Details.size());
+        //System.out.println(Repos_id.size()+","+repo_nameL.size()+", "+owner_nameL.size()+", "+full_nameL.size()+", "+depth.size()+", "+Commits.size()+","
+        //+TotalDependencies.size()+", "+CommitsAfter.size()+", "+DependAfter.size()+", "+Created_at.size()+", "+DependAfter_Details.size());
         List<Double> list_id = new ArrayList<>();
         List<String> list_set = new ArrayList<>();
         //list_set.addAll(set_set);
@@ -52,7 +52,7 @@ public class Jaccard {
         Object[] datas = null;
 
         ArrayList< Object[]> allobj = new ArrayList<Object[]>();
-        datas = new Object[]{"Repos_id", "Reos_name", "fullname", "Owner_name", "Created_at", "Depth", "Commits", "TotalDependencies", "Dependencies", "", "CommitsAfter", "DependAfter", "DependAfter_Details", "JaccardDistance", "JaccardSimmilarity"};
+        datas = new Object[]{"Repos_id", "Reos_name", "fullname", "Owner_name", "Created_at", "Depth", "Commits", "TotalDependencies", "Dependencies", "", "CommitsAfter", "DependAfter", "DependAfter_Details", "JaccardDistance", "JaccardSimmilarity","Group"};
         allobj.add(datas);
 
         for (int i = 0; i < depth.size(); i++) {
@@ -84,13 +84,17 @@ public class Jaccard {
                     double jaccardDistance = new org.apache.commons.text.similarity.JaccardDistance().apply(MLP, FP);
                     double jaccardSimilarity = new org.apache.commons.text.similarity.JaccardSimilarity().apply(MLP, FP);
 
-                    datas = new Object[]{Repos_id.get(j), repo_nameL.get(j), full_nameL.get(j), owner_nameL.get(j), Created_at.get(j), depth.get(j),  Commits.get(j), TotalDependencies.get(j), Dependencies.get(j), "", CommitsAfter.get(j), DependAfter.get(j), DependAfter_Details.get(j), jaccardDistance, jaccardSimilarity};
+                    int group = 1;
+                    if (jaccardDistance <= 0.50) {
+                        group = 0;
+                    }
+                    datas = new Object[]{Repos_id.get(j), repo_nameL.get(j), full_nameL.get(j), owner_nameL.get(j), Created_at.get(j), depth.get(j),  Commits.get(j), TotalDependencies.get(j), Dependencies.get(j), "", CommitsAfter.get(j), DependAfter.get(j), DependAfter_Details.get(j), jaccardDistance, jaccardSimilarity, group};
                     allobj.add(datas);
 
                 }
             }
 
-            Create_Excel.createExcel2(allobj, 0, path + "ReposFork_Dependencies_version_jaccard_003.xlsx", "jaccard");
+            Create_Excel.createExcel2(allobj, 0, path + "ReposFork_Dependencies_version_jaccard_0003.xlsx", "jaccard");
 
         }
 
